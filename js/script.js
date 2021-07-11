2af35572187ac285ba4bb1a99e2dc7ed46d112f7
Cyril Vermande <cyril@cyrilwebdesign.com>
2021-07-11 20:02:10 +0200

    Use HTML templates

--- js/script.js
+++ js/script.js
@@ -4,73 +4,59 @@
     }).then(function(response) {
 		/* summary */
 		document.getElementById('summary').innerText = response.basics.summary;
+
+        /* skills */
+        var skillTemplate = document.getElementById('skill-template');
+        for(var i in response.skills){
+            var newSkill = document.importNode(skillTemplate.content, true);
+            newSkill.querySelector('article h3').innerText = response.skills[i].name;
+            for(var j in response.skills[i].keywords){
+                var newKeyword = document.createElement('li');
+                newKeyword.innerHTML = '<i class="fa fa-chevron-right"></i> ' + response.skills[i].keywords[j];
+                newSkill.querySelector('article ul').append(newKeyword);
+            }
+            document.getElementById('skills').appendChild(newSkill);
+        }
 		
 		/* experience */
-		var experiences = '', experienceTemplate = document.getElementById('experience-template').innerHTML;
-		response.work.sort(function(a,b){
-			return a.startDate < b.startDate;
-		});
-		for(var i in response.work){
-			experiences += experienceTemplate
-				.replace('{{ year }}', response.work[i].startDate.replace(/\D*/, ''))
-				.replace('{{ position }}', response.work[i].position)
-				.replace('{{ company }}', response.work[i].company)
-				.replace('{{ website }}', response.work[i].website)
-				.replace('{{ summary }}', response.work[i].summary)
-				.replace('{{ highlights }}', function(){
-					var html = '';
-					for(var j in response.work[i].highlights){
-						html += '<li><i class="fa fa-chevron-right"></i> ' + response.work[i].highlights[j] + '</li>';
-					}
-					return html;
-				})
-			;
-		}
-        document.getElementById('experience').innerHTML = experiences;
+        var experienceTemplate = document.getElementById('experience-template');
+        for(var i in response.work) {
+            var newExperience = document.importNode(experienceTemplate.content, true);
+            newExperience.querySelector('article .disk').innerText = response.work[i].startDate.replace(/\D*/, '');
+            newExperience.querySelector('article h3 a').innerText = response.work[i].company;
+            newExperience.querySelector('article h4').innerText = response.work[i].position;
+            newExperience.querySelector('article p').innerText = response.work[i].summary;
+            for(var j in response.work[i].highlights){
+                var newKeyword = document.createElement('li');
+                newKeyword.innerHTML = '<i class="fa fa-chevron-right"></i> ' + response.work[i].highlights[j] ;
+                newExperience.querySelector('article ul').append(newKeyword);
+            }
+            document.getElementById('experience').appendChild(newExperience);
+        }
 		
 		/* education */
-		var education = experienceTemplate
-			.replace('{{ year }}', response.education[0].endDate.replace(/\D*/, ''))
-			.replace('{{ position }}', response.education[0].area)
-			.replace('{{ company }}', response.education[0].institution)
-			.replace('{{ website }}', '#')
-			.replace('{{ summary }}', '')
-			.replace('{{ highlights }}', function(){
-				var html = '';
-				for(var j in response.education[0].courses){
-					html += '<li><i class="fa fa-chevron-right"></i> ' + response.education[0].courses[j] + '</li>';
-				}
-				return html;
-			})
-		;
-        document.getElementById('education').innerHTML = education;
-		
-		/* skills */
-		var skills = '', skillTemplate = document.getElementById('skill-template').innerHTML;
-		for(var i in response.skills){
-			skills += skillTemplate
-				.replace('{{ name }}', response.skills[i].name)
-				.replace('{{ keywords }}', function(){
-					var html = '';
-					for(var j in response.skills[i].keywords){
-						html += '<li><i class="fa fa-chevron-right"></i> ' + response.skills[i].keywords[j] + '</li>';
-					}
-					return html;
-				})
-			;
-		}
-        document.getElementById('skills').innerHTML = skills;
+        var newEducation = document.importNode(experienceTemplate.content, true);
+        newEducation.querySelector('article .disk').innerText = response.education[0].endDate.replace(/\D*/, '');
+        newEducation.querySelector('article h3').innerText = response.education[0].institution;
+        newEducation.querySelector('article h4').innerText = response.education[0].area;
+        newEducation.querySelector('article p').remove();
+        for(var j in response.education[0].courses){
+            var newKeyword = document.createElement('li');
+            newKeyword.innerHTML = '<i class="fa fa-chevron-right"></i> ' + response.education[0].courses[j] ;
+            newEducation.querySelector('article ul').append(newKeyword);
+        }
+        document.getElementById('education').appendChild(newEducation);
 		
 		/* references */
-		var references = '', referenceTemplate = document.getElementById('reference-template').innerHTML;
-		for(var i in response.references){
-			references += referenceTemplate
-				.replace('{{ name }}', response.references[i].name)
-				.replace('{{ logo }}', response.references[i].logo)
-				.replace('{{ website }}', response.references[i].website)
-			;
-		}
-        document.getElementById('references').innerHTML = references;
+        var referenceTemplate = document.getElementById('reference-template');
+        for(var i in response.references){
+            var newReference = document.importNode(referenceTemplate.content, true);
+            newReference.querySelector('article a').setAttribute('href', response.references[i].website);
+            newReference.querySelector('article img').setAttribute('src', response.references[i].logo);
+            newReference.querySelector('article img').setAttribute('alt', response.references[i].name);
+            newReference.querySelector('article img').setAttribute('title', response.references[i].name);
+            document.getElementById('references').appendChild(newReference);
+        }
 	});
 
 	/* email */
